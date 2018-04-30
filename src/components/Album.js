39024f2ba4885b939cf4c98477b49bd258261635
spjohnson <spgant@gmail.com>
2018-04-30 07:16:00 -0600

    revision request fixed and album covers now showing up

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'; 
+import albumData from './../data/albums'; 
 
 class Album extends Component {
+	constructor(props) {
+		super(props); 
+	
+
+	const album = albumData.find( album => {
+		return album.slug === this.props.match.params.slug
+	}); 
+
+	this.state = {
+		album: album
+	}; 
+}
+
 	render() {
 		return (
 			<section className="album">
-				{this.props.match.params.slug} Album will go here 
+			{this.props.match.params.slug} Album will go here
 			</section>
 		); 
 	}
