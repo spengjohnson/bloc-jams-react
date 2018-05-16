f890f726b059a86df30f85646459f1caff9d589f
spjohnson <spgant@gmail.com>
2018-05-15 18:07:08 -0600

    designed Bloc Jams

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import './landing.css'; 
 
 const Landing = () => (
 	<section className="landing">
@@ -11,7 +12,7 @@ const Landing = () => (
 	</div>
 		<div className="point">
 		<h2 className="point-title">Unlimited, streaming, ad-free</h2>
-		<p className="point-description">No arbitraty limits. No distractions.</p>
+		<p className="point-description">No arbitrary limits. No distractions.</p>
 	</div>
 		<div className="point">
 		<h2 className="point-title">Mobile enabled</h2>
